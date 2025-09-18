6b9312ab10cad6eadee15a288bba837328add8d5
HotIcecube88 <connormcneely@icloud.com>
2025-09-17 22:02:31 -0500

    Add files via upload

---
+++ homework_hub_pwa/sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'hh-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const { request } = e;
+  if (request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(request).then(cached => cached || fetch(request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(cache => cache.put(request, copy));
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
